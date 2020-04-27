6ef7fa8300b9ab97c29703df22a04c2675cc08cf
bravers <bravers@yandex.ru>
2020-04-27 15:31:25 +0300

    Script for creating file with text

---
+++ Manage_drives_and_files/New-FileWithText.ps1
@@ -0,0 +1,4 @@
+﻿$fileName = "CreateNewFile.txt"
+$textForFile = "This file is created using a script that also writes text to a file."
+New-Item -Path "." -Name $fileName -ItemType File -Force
+$textForFile | Out-File $fileName 
