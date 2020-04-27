cd441364876488c267ee074decce36576597baca
bravers <bravers@yandex.ru>
2020-04-27 14:56:59 +0300

    Ready script ComputerInfo

---
+++ Get-ComputerInfo.ps1
@@ -0,0 +1,12 @@
+﻿function Set-InfoToFile{
+	Get-Date
+	Get-ComputerInfo -Property "Windows*" 	
+}
+$fpath = "ComputerInfo.txt"
+$isfile = Test-Path $fpath 
+if ($isfile){
+	Write-Host "Такой файл уже существует"
+}
+else {
+	Set-InfoToFile | Out-File $fpath
+}
