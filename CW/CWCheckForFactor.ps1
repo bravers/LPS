e1819becaebb5bfa7c5f3404ea13d6d2d0eddbd2
bravers <bravers@yandex.ru>
2020-05-22 03:41:32 +0300

    1

---
+++ CW/CWCheckForFactor.ps1
@@ -0,0 +1,19 @@
+﻿<#
+This function should test if the factor is a factor of base.
+
+Return true if it is a factor or false if it is not.
+#>
+
+function CheckForFactor([int] $base, [int] $factor) {
+  if ($base%$factor -eq 0){
+    $true
+  }
+  else{
+    $false
+  }
+}
+
+$b = 15
+$f = 3
+
+CheckForFactor $b $f
---
+++ CW/CWTwoToOne.ps1
@@ -0,0 +1,19 @@
+﻿<#
+	Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters.
+	a = "xyaabbbccccdefww"
+	b = "xxxxyyyyabklmopq"
+	longest(a, b) -> "abcdefklmopqwxy"
+
+	a = "abcdefghijklmnopqrstuvwxyz"
+	longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+#>
+function longest($a1, $a2)
+{
+    return($c = (([string]::Concat($a1,$a2)).ToCharArray() | Sort-Object -Unique) -join "")   
+}
+
+$a1 = "xyaabbbccccdefww"
+$a2 = "xxxxyyyyabklmopqz"
+
+longest $a1 $a2
+
